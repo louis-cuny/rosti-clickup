6c5e7ec32144f995d51da600b868a16acb5d3cfb
louis-cuny <louis-cuny@orange.fr>
2023-02-04 20:02:16 +0100

    Retrieve more than first page result tasks

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::env;
+use std::error::Error;
+
 use chrono::{DateTime, Datelike, Duration, NaiveDate, NaiveDateTime, Utc, Weekday};
 use chronoutil::delta::shift_months;
 use dotenv::dotenv;
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 use reqwest::Client;
-use std::borrow::Borrow;
-use std::collections::HashMap;
-use std::env;
-use std::error::Error;
 
 const FRENCH_MONTHS: [&'static str; 12] = [
     "Janvier",
@@ -69,6 +70,53 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+async fn get_billable_tasks(base_endpoint: String, api_key: &str) -> HashMap<String, Task> {
+    let client: Client = Client::new();
+    let mut tasks: HashMap<String, Task> = HashMap::new();
+    let mut count = 0u32;
+
+    loop {
+        let tasks_result: serde_json::Value = client
+            .get(format!("{}{}", base_endpoint, count))
+            .header(AUTHORIZATION, api_key)
+            .header(CONTENT_TYPE, "application/json")
+            .send()
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap();
+
+        let raw_tasks = tasks_result["tasks"].as_array().unwrap();
+        if raw_tasks.len() < 1 {
+            break;
+        }
+
+        for t in raw_tasks.iter() {
+            tasks.insert(
+                t.get("id").unwrap().as_str().unwrap().to_string(),
+                Task {
+                    custom_id: t.get("custom_id").unwrap().as_str().unwrap().to_string(),
+                    name: t.get("name").unwrap().as_str().unwrap().to_string(),
+                    duration: 0,
+                    list_id: t
+                        .get("list")
+                        .unwrap()
+                        .get("id")
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
+                        .to_string(),
+                },
+            );
+        }
+
+        count += 1;
+    }
+
+    return tasks;
+}
+
 async fn per_months(api_key: &str, org_id: &str) -> Result<(), Box<dyn Error>> {
     let now: DateTime<Utc> = Utc::now();
     let mut current_month: NaiveDateTime =
@@ -79,37 +127,8 @@ async fn per_months(api_key: &str, org_id: &str) -> Result<(), Box<dyn Error>> {
 
     let mut endpoint_tasks: String = "https://api.clickup.com/api/v2/team/".to_owned();
     endpoint_tasks.push_str(org_id);
-    endpoint_tasks.push_str("/task?include_closed=1&custom_fields%5B%5D=%5B%7B%22field_id%22%3A%20%22e2407157-67b9-4edc-964b-19550176c7ee%22%2C%22operator%22%3A%20%22%3D%22%2C%20%22value%22%3A%20%220%22%7D%5D");
-
-    let tasks_result: serde_json::Value = client
-        .get(endpoint_tasks)
-        .header(AUTHORIZATION, api_key)
-        .header(CONTENT_TYPE, "application/json")
-        .send()
-        .await?
-        .json()
-        .await
-        .unwrap();
-
-    let mut tasks: HashMap<String, Task> = HashMap::new();
-    for t in tasks_result["tasks"].as_array().unwrap().iter() {
-        tasks.insert(
-            t.get("id").unwrap().as_str().unwrap().to_string(),
-            Task {
-                custom_id: t.get("custom_id").unwrap().as_str().unwrap().to_string(),
-                name: t.get("name").unwrap().as_str().unwrap().to_string(),
-                duration: 0,
-                list_id: t
-                    .get("list")
-                    .unwrap()
-                    .get("id")
-                    .unwrap()
-                    .as_str()
-                    .unwrap()
-                    .to_string(),
-            },
-        );
-    }
+    endpoint_tasks.push_str("/task?include_closed=1&custom_fields%5B%5D=%5B%7B%22field_id%22%3A%20%22e2407157-67b9-4edc-964b-19550176c7ee%22%2C%22operator%22%3A%20%22%3D%22%2C%20%22value%22%3A%20%220%22%7D%5D&page=");
+    let mut tasks = get_billable_tasks(endpoint_tasks, api_key).await;
 
     println!("\n\x1b[1;93mÀ facturer / total\x1b[0m");
 
